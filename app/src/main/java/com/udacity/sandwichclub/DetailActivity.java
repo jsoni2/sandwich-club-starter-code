318618bb359d1ad87268ab2e92ba2ef8611893dc
sonij02 <sonij02@thryv.com>
2020-01-21 15:54:01 -0600

    Submission1

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,12 +16,18 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    private TextView mOrigin, mAlsoKnownAs, mIngredients, mDescription;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        mOrigin = findViewById(R.id.origin_tv);
+        mAlsoKnownAs = findViewById(R.id.also_known_tv);
+        mIngredients = findViewById(R.id.ingredients_tv);
+        mDescription = findViewById(R.id.description_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +50,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -51,12 +58,21 @@ public class DetailActivity extends AppCompatActivity {
         setTitle(sandwich.getMainName());
     }
 
+    private void populateUI(Sandwich sandwich) {
+        mOrigin.setText(sandwich.getPlaceOfOrigin());
+        mDescription.setText(sandwich.getDescription());
+        for (String ingredient : sandwich.getIngredients()) {
+            mIngredients.append((ingredient) + "\n");
+        }
+        for (String alias : sandwich.getAlsoKnownAs()) {
+            mAlsoKnownAs.append((alias) + "\n");
+        }
+
+    }
+
     private void closeOnError() {
         finish();
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
-
-    }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,40 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        JSONObject obj = null;
+        Sandwich sandwich = new Sandwich();
+        try {
+            obj = new JSONObject(json);
+            sandwich.setMainName(obj.getJSONObject("name").getString("mainName"));
+            JSONArray arrAlsoKnownAs = obj.getJSONObject("name").getJSONArray("alsoKnownAs");
+            List<String> alsoKnownAs = new ArrayList<String>();
+            for(int i = 0; i < arrAlsoKnownAs.length(); i++){
+                alsoKnownAs.add((String) arrAlsoKnownAs.get(i));
+            }
+            sandwich.setAlsoKnownAs(alsoKnownAs);
+            sandwich.setPlaceOfOrigin(obj.getString("placeOfOrigin"));
+            sandwich.setDescription(obj.getString("description"));
+            sandwich.setImage(obj.getString("image"));
+            JSONArray arrIngredients = obj.getJSONArray("ingredients");
+            List<String> ingredients = new ArrayList<String>();
+            for(int i = 0; i < arrIngredients.length(); i++){
+                ingredients.add((String) arrIngredients.get(i));
+            }
+            sandwich.setIngredients(ingredients);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return sandwich;
     }
 }
