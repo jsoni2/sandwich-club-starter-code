fb288b78b0f696ecc5e365b4fac5f12eb154778a
sonij02 <sonij02@thryv.com>
2020-01-22 10:47:21 -0600

    show an alternate image when any error occurs during the loading time of the image or image is not present

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -53,6 +53,8 @@ public class DetailActivity extends AppCompatActivity {
         populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
+                .placeholder(R.drawable.ic_error_black_24dp)
+                .error(R.drawable.ic_error_black_24dp)
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
